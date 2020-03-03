8f0eabd317ecfc1adb32e714ca04c0e8f1a4a517
hyunwook429 <hyunwook429@naver.com>
2020-03-03 14:44:25 +0900

    Updates

---
+++ precache-manifest.0ddf122afb758f5051c0228c05c5fe6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796c7d037ded4e38dc9cd6a48460b952",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "101cb10b6fda6f38c380",
+    "url": "/movie_app/static/css/main.9a95ece9.chunk.css"
+  },
+  {
+    "revision": "239a85fd936fd6651937",
+    "url": "/movie_app/static/js/2.c760581d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.c760581d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101cb10b6fda6f38c380",
+    "url": "/movie_app/static/js/main.312d5875.chunk.js"
+  },
+  {
+    "revision": "c0057b52e72b0f614c37",
+    "url": "/movie_app/static/js/runtime-main.15be7292.js"
+  }
+]);
